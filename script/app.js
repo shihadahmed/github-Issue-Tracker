26484814422ff3297430f5e923d81fca398a80db
shihadahmed <shihadbinahmed@gmail.com>
2026-04-27 23:29:22 +0600

    Sign In Page Complate

---
+++ script/app.js
@@ -0,0 +1,30 @@
+
+// Form element select kora
+const myForm = document.getElementById('loginForm');
+const errorText = document.getElementById('error-message'); // Error element ta dhorlam
+
+// Login logic function
+function checkLogin(e) {
+    e.preventDefault(); 
+
+    let user = document.getElementById('username').value;
+    let pass = document.getElementById('password').value;
+
+    // Credentials check
+    if (user === 'admin' && pass === 'admin123') {
+        localStorage.setItem('isLoggedIn', 'true');
+        window.location.href = 'home.html'; 
+    } else {
+        errorText.innerHTML = 'Invalid Username or Password! </br> Please use the demo credentials.'
+        errorText.classList.remove('hidden');
+        
+        // 3 second por error message ta automatic muche jabe
+        setTimeout(() => {
+            errorText.classList.add('hidden');
+        }, 3000);
+    }
+
+// Form event listener
+if (myForm) {
+    myForm.addEventListener('submit', checkLogin);
+}
